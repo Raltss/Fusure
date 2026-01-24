d4bb11b776aff967b96e139e1aa29b645da9e6bd
Manuel <barcelonamanuelmarc@gmail.com>
2026-01-24 09:38:18 +0800

    Create user registration
    Design registration form UI with DaisyUI
    
    Create registration API endpoint
    
    Implement form validation
    
    Handle registration errors
    
    Redirect after successful registration

--- job-board-frontend/src/router/index.ts
+++ job-board-frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TestApi from '@/views/TestApi.vue'
+import RegisterView from '@/views/auth/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/test',
       name: 'test',
       component: () => TestApi,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => RegisterView,
     },
     {
       path: '/about',
